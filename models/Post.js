05316bab39ce11d13653499ebc1265f54ceb3acb
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2020-06-19 12:43:23 +0530

    backend setup done

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.Object,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  media: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
